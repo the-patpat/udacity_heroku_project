e71707a746be2dfbf68e6e3890f03be36b9a233b
Patrick Schmidt <patschmidt1234@gmail.com>
2022-10-24 16:40:41 +0200

    Skeleton code added

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.linear_model import LogisticRegression
 
 
 # Optional: implement hyperparameter tuning.
@@ -17,8 +18,8 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-
-    pass
+    lrc = LogisticRegression(random_state=42).fit(X_train, y_train)
+    return lrc
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +58,5 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict_proba(X)
+    return preds
--- starter/train_model.py
+++ starter/train_model.py
@@ -1,13 +1,16 @@
 # Script to train machine learning model.
 
+import joblib
+import pandas as pd
 from sklearn.model_selection import train_test_split
-
-# Add the necessary imports for the starter code.
+from starter.ml.data import process_data
+from starter.ml.model import train_model
 
 # Add code to load in the data.
+census_df = pd.read_csv('data/census.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(census_df, test_size=0.20)
 
 cat_features = [
     "workclass",
@@ -24,5 +27,10 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label='salary', training=False
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+joblib.dump(model, 'lrc_census.joblib')
