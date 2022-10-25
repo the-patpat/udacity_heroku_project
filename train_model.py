bca281f48691f5eb7cedfc1e08cf6d9d7c0f060c
Patrick Schmidt <patschmidt1234@gmail.com>
2022-10-25 11:45:18 +0200

    Small typo

--- train_model.py
+++ train_model.py
@@ -21,7 +21,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("logs/process_data.log"),
+        logging.FileHandler("logs/train_model.log"),
         logging.StreamHandler()
     ]
 )
