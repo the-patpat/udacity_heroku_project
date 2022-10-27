f8f1aa0e32667858db628f8b362e097da9df22c2
Patrick Schmidt <patschmidt1234@gmail.com>
2022-10-27 13:14:15 +0200

    added stuff for heroku

--- main.py
+++ main.py
@@ -146,6 +146,12 @@ logging.basicConfig(
     ]
 )
 
+# Need to pull data if we're on heroku
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 logging.info('Loading LabelBinarizer')
 lb = joblib.load(
