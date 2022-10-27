dfa7ca5865e78e3aeb9a7764efe7ce0102923e12
Patrick Schmidt <patschmidt1234@gmail.com>
2022-10-27 14:14:38 +0200

    fixed typo

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from utils import MakeFileHandler
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     os.system("dvc config core.hardlink_lock true")
-    os.system("dvc pull -r driveremote"):
+    os.system("dvc pull -r driveremote")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 import dvc.api
